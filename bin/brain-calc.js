b16ede1a02639e2b314320d0dbe344287c334fb0
Настя Палажченко <nastyapalazhchenko@MacBook-Pro-Nasta.local>
2024-08-06 23:31:33 +0300

    Fix errors

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import playing from '../src/index.js';
-import brainCalc from '../games/brainСalc.js';
+import brainCalc from '../games/brainCalc.js';
 
 const whatToDo = 'What is the result of the expression?';
 
